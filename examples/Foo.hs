e2019eae4c220768ac714fe211ac2111f60860e1
Jonathan Lange <jml@mumak.net>
2017-09-23 10:11:36 +0100

    Provide structured examples
    - single binary
    - single module library
    - one module that depends on another (FAILING)

--- examples/Foo.hs
+++
@@ -1,6 +0,0 @@
-module Examples.Foo where
-
-import Data.List
-import Examples.Ch4_3
-
-bar = "yep"
--- examples/Hello/main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import Examples.Ch4_3
-
-main = do
-    putStrLn "hello world"
-    -- print foo.bar
-    print awesome
